4c7100af4deb6b7cbf3f6a652925dcd9b57482bf
aqqz <1251384901@qq.com>
2023-03-30 22:40:23 +0800

    update file

---
+++ loss.py
@@ -0,0 +1,179 @@
+import torch.nn as nn
+import torch
+
+class YOLO_Loss(nn.Module):
+
+    def __init__(self, S=7, B=2, C=20, l_coord=5, l_noobj=0.5, input_size=224, device='cuda') -> None:
+        super().__init__()
+
+        self.S = S
+        self.B = B
+        self.C = C
+        self.l_coord = l_coord
+        self.l_noobj = l_noobj
+        self.input_size = input_size
+        self.device = device
+
+
+    def forward(self, y_pred, y_true):
+
+        self.response_mask = y_true[..., 0] #(?, 7, 7)
+        gt_box = y_true[..., 1:5] #(?, 7, 7, 4)
+        gt_cls = y_true[..., 5:] #(?, 7, 7, 20)
+
+        pred_con1 = y_pred[..., 0]
+        pred_box1 = y_pred[..., 1:5]
+        pred_con2 = y_pred[..., 5]
+        pred_box2 = y_pred[..., 6:10]
+        pred_cls = y_pred[..., 10:]
+        
+        gt_box_trans = self.box_trans(gt_box)
+        pred_box1_trans = self.box_trans(pred_box1)
+        pred_box2_trans = self.box_trans(pred_box2)
+        
+        box1_iou = self.iou(gt_box_trans, pred_box1_trans)
+        box2_iou = self.iou(gt_box_trans, pred_box2_trans)
+        best_iou = torch.max(box1_iou, box2_iou)
+        
+        box1_mask = self.response_mask * (box1_iou == best_iou) #(?, 7, 7)
+        box2_mask = self.response_mask * (box2_iou == best_iou)
+        
+        xy_loss = box1_mask * torch.sum(torch.square(pred_box1[..., 0:2] - gt_box[..., 0:2]), dim=3) + \
+            box2_mask * torch.sum(torch.square(pred_box2[..., 0:2] - gt_box[..., 0:2]), dim=3)
+        xy_loss = torch.sum(xy_loss)
+        
+        wh_loss = box1_mask * torch.sum(torch.square(torch.sqrt(pred_box1[..., 2:4] + 1e-5) - torch.sqrt(gt_box[..., 2:4] + 1e-5)), dim=3) + \
+            box2_mask * torch.sum(torch.square(torch.sqrt(pred_box2[..., 2:4] + 1e-5) - torch.sqrt(gt_box[..., 2:4] + 1e-5)), dim=3)
+        wh_loss = torch.sum(wh_loss)
+        
+        ob_loss = box1_mask * torch.square(pred_con1 - best_iou) + \
+            box2_mask * torch.square(pred_con2 - best_iou)
+        ob_loss = torch.sum(ob_loss)
+        
+        no_loss = (1 - box1_mask) * torch.square(pred_con1 - 0) + \
+            (1-box2_mask) * torch.square(pred_con2 - 0)
+        no_loss = torch.sum(no_loss)
+        
+        cls_loss = self.response_mask * torch.sum(torch.square(pred_cls - gt_cls), dim=3)
+        cls_loss = torch.sum(cls_loss)
+        
+        total_loss = self.l_coord * xy_loss + \
+            self.l_coord * wh_loss + \
+            ob_loss + \
+            self.l_noobj * no_loss + \
+            cls_loss
+        
+        return total_loss / y_pred.shape[0]
+        
+        
+        
+        
+    def box_trans(self, box_ori):
+        """
+
+        Args:
+            box_ori (tensor): [x, y, w, h] x,y relative to grid, w,h relative to whole image 
+        Return:
+            box_trans (tensor): [xmin, ymin, xmax, ymax] relative to whole image.
+        """
+        batch = box_ori.shape[0]
+        x, y, w, h = box_ori[..., 0], box_ori[..., 1], box_ori[..., 2], box_ori[..., 3] #(?, 7, 7)
+        
+        offset_x = torch.tensor([[i for i in range(self.S)]*self.S*batch], device=self.device).view(batch, self.S, self.S) #(?, 7, 7)
+        offset_y = torch.transpose(offset_x, dim0=1, dim1=2)
+        
+        x = (x + offset_x) / self.S
+        y = (y + offset_y) / self.S
+        
+        xmin = x - w / 2.0
+        ymin = y - h / 2.0
+        xmax = x + w / 2.0
+        ymax = y + h / 2.0 
+    
+        box_trans = torch.stack([xmin, ymin, xmax, ymax], dim=3)
+        
+        return box_trans
+    
+    
+    
+    def iou(self, box1, box2):
+        """_summary_
+
+        Args:
+            box1 (tensor): [x11, y11, x12, y12]
+            box2 (tensor): [x21, y21, x22, y22]
+            
+            
+          11------------------
+            |                |
+            |                |
+            |  21(a)---------------------
+            |       |        |          |
+            |       |        |          |
+            ------------------12(b)     |
+                    |                   |
+                    |                   |
+                    ---------------------22
+
+        """
+        
+        x11, y11, x12, y12 = box1[..., 0], box1[..., 1], box1[..., 2], box1[..., 3] #(?, 7, 7)
+        x21, y21, x22, y22 = box2[..., 0], box2[..., 1], box2[..., 2], box2[..., 3]
+        
+        xa = torch.max(x11, x21)
+        ya = torch.max(y11, y21)
+        xb = torch.min(x12, x22)
+        yb = torch.min(y12, y22)
+        
+        w = torch.max(xb-xa, torch.zeros_like(xb))
+        h = torch.max(yb-ya, torch.zeros_like(yb))
+        
+        s1 = (x12-x11)*(y12-y11)
+        s2 = (x22-x21)*(y22-y21)
+        
+        s_inner = w*h
+        
+        iou = s_inner / (s1 + s2 - s_inner + 1e-5)
+        
+        
+        return iou
+        
+    
+        
+        
+        
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    
+    from dataset import MyDataset
+    from voc_dataset import VOC_Dataset
+    from model import YOLO_Model
+    import torchvision.transforms as T
+    
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Resize([224, 224]),
+    ])
+    
+    train_ds = VOC_Dataset(image_root="/home/taozhi/datasets/VOC0712/JPEGImages/",
+                        label_root="/home/taozhi/datasets/VOC0712/Annotations/",
+                        txt_file="/home/taozhi/datasets/VOC0712/0712trainval.txt",
+                        transform=transform)
+    image, label = train_ds.__getitem__(0)
+    image, label = image.unsqueeze(0), label.unsqueeze(0)
+    print(image.shape, label.shape)
+    
+    model = YOLO_Model(S=7,B=2,C=20)
+    output = model(image)
+    
+    loss_fn = YOLO_Loss(S=7,B=2,C=20,l_coord=5,l_noobj=0.5,input_size=224, device='cpu')
+    
+    loss = loss_fn(output, label)
+    print(loss)
+        
+
---
+++ predict.py
@@ -0,0 +1,248 @@
+import torch, os
+from dataset import MyDataset
+from voc_dataset import VOC_Dataset
+from model import YOLO_Model
+import torchvision.transforms as T
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+class YOLO_Predictor():
+    
+    def __init__(self, model, test_ds: MyDataset, score_threshold=0.5, iou_threshold=0.5, device='cpu') -> None:
+        self.model = model
+        self.test_ds = test_ds
+        self.score_threshold = score_threshold
+        self.iou_threshold = iou_threshold
+        self.device = device
+        self.S = self.test_ds.S
+        self.B = self.test_ds.B
+        self.C = self.test_ds.C
+        self.input_size = self.test_ds.input_size
+        self.class_list = self.test_ds.class_list
+        self.color_list = self.test_ds.color_list
+        self.transform = self.test_ds.transform
+    
+    
+    def predict(self, index, path=None):
+        
+        #use test dataset
+        if type(self.test_ds) == MyDataset:
+            image_name = self.test_ds.filelist[index] + '.jpeg'
+        else:
+            image_name = self.test_ds.filelist[index] + '.jpg'
+        image_path = os.path.join(self.test_ds.image_root, image_name)
+        pil_image = Image.open(image_path).convert('L')
+        image_array = np.array(pil_image)
+        height, width = image_array.shape[0], image_array.shape[1]
+        
+        self.model.eval()
+        image, _ = self.test_ds.__getitem__(index)
+        
+        
+        #use other image
+        if path != None:
+            pil_image = Image.open(path).convert('L')
+            image_array = np.array(pil_image)
+            height, width = image_array.shape[0], image_array.shape[1]
+            
+            self.model.eval()
+            image = self.transform(image_array)
+        
+        input = image.unsqueeze(0)
+        output = self.model(input)
+        ob_infos = self.post_progress(output[0], height, width)
+        print(ob_infos)
+        
+        self.draw(image_array, ob_infos)
+        
+        
+        
+    
+    def post_progress(self, output_tensor, ori_h, ori_w):
+        
+        box_before = []
+        
+        for i in range(self.S):
+            for j in range(self.S):
+                grid_vector = output_tensor[i, j]
+                box1_con = grid_vector[0].item()
+                box1_x = grid_vector[1].item()
+                box1_y = grid_vector[2].item()
+                box1_w = grid_vector[3].item()
+                box1_h = grid_vector[4].item()
+                box2_con = grid_vector[5].item()
+                box2_x = grid_vector[6].item()
+                box2_y = grid_vector[7].item()
+                box2_w = grid_vector[8].item()
+                box2_h = grid_vector[9].item()
+                box_cls = grid_vector[10:]
+                
+                box_id = torch.argmax(box_cls).item()
+                box1_score = box1_con * box_cls[box_id].item()
+                box2_score = box2_con * box_cls[box_id].item()
+                
+                if box1_score >= self.score_threshold:
+                    box1_xmin, box1_ymin, box1_xmax, box1_ymax = self.box_trans(box1_x, box1_y, box1_w, box1_h, j, i, ori_h, ori_w)
+                    box_before.append([box_id, box1_score, box1_xmin, box1_ymin, box1_xmax, box1_ymax])
+                
+                if box2_score >= self.score_threshold:
+                    box2_xmin, box2_ymin, box2_xmax, box2_ymax = self.box_trans(box2_x, box2_y, box2_w, box2_h, j, i, ori_h, ori_w)
+                    box_before.append([box_id, box2_score, box2_xmin, box2_ymin, box2_xmax, box2_ymax])
+                    
+              
+        box_after = self.nms(box_before)
+        
+        return box_after
+        
+            
+                
+                
+    def box_trans(self, x, y, w, h, offset_x, offset_y, ori_h, ori_w):
+        """box trans from [x, y, w, h] to [xmin, ymin, xmax, ymax] real coord
+
+        Args:
+            x (_type_): _description_
+            y (_type_): _description_
+            w (_type_): _description_
+            h (_type_): _description_
+            offset_x (_type_): _description_
+            offset_y (_type_): _description_
+        """
+        
+        x = ( x + offset_x ) / self.S * self.input_size
+        y = ( y + offset_y ) / self.S * self.input_size
+        w = w * self.input_size
+        h = h * self.input_size
+        
+        x = x * ori_w / self.input_size
+        y = y * ori_h / self.input_size
+        w = w * ori_w / self.input_size
+        h = h * ori_h / self.input_size
+        
+        xmin = x - w / 2.0
+        ymin = y - h / 2.0
+        xmax = x + w / 2.0
+        ymax = y + h / 2.0
+        
+        return xmin, ymin, xmax, ymax
+                
+                    
+                    
+    def nms(self, box_before: list):
+        
+        box_after = []
+        box_before.sort(key=lambda x:x[1], reverse=True)
+        
+        while len(box_before) > 0:
+            box_after.insert(0, box_before[0])
+            box_before.remove(box_before[0])
+            
+            for box in box_before:
+                test_iou = self.iou(box[2:], box_after[0][2:])
+                if test_iou >= self.iou_threshold and box[0] == box_after[0][0]:
+                    box_before.remove(box)
+        
+        return box_after
+        
+                   
+                
+        
+        
+    def iou(self, box1, box2):
+        """compute iou with box1 an box2
+
+        Args:
+            box1 (list): [x11, y11, x12, y12]
+            box2 (list): [x21, y21, x22, y22]
+        """
+    
+        x11, y11, x12, y12 = box1[0], box1[1], box1[2], box1[3]
+        x21, y21, x22, y22 = box2[0], box2[1], box2[2], box2[3]
+        
+        xa = max(x11, x21)
+        ya = max(y11, y21)
+        xb = min(x12, x22)
+        yb = min(y12, y22)
+        
+        w = max(xb-xa, 0)
+        h = max(yb-ya, 0)
+        
+        s1 = (x12-x11)*(y12-y11)
+        s2 = (x22-x21)*(y22-y21)
+        s_inner = w*h
+        
+        iou = s_inner / (s1 + s2 - s_inner + 1e-5)
+    
+        return iou
+    
+    
+    
+    
+    def draw(self, image_array, ob_infos):
+        
+        fig, ax = plt.subplots()
+        if type(self.test_ds) == MyDataset:
+            ax.imshow(image_array, cmap='gray')
+        else:
+            ax.imshow(image_array, cmap="gray")
+        
+        for ob in ob_infos:
+            cls, score, xmin, ymin, xmax, ymax = ob[0], ob[1], int(ob[2]), int(ob[3]), int(ob[4]), int(ob[5])
+            
+            rect = mpatches.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin, fill=0, color=self.color_list[cls])
+            ax.add_patch(rect)
+            ax.text(xmin-2, ymin-2, s=self.class_list[cls] + ': ' + str(score)[:4], color=self.color_list[cls])
+        
+        plt.show()
+    
+    
+    def save_samples(self, im_count):
+        out_dir = 'samples/'
+
+        if os.path.exists(out_dir) == False:
+            os.mkdir(out_dir)
+            
+        cnt = 0
+        for image, label in val_ds:
+            im_name = str(cnt) + '.pgm'
+            from torchvision.utils import save_image
+            save_image(image, open(os.path.join(out_dir, im_name), "wb"))
+            cnt += 1
+            if cnt >= im_count:
+                break
+            
+            
+    def onnx_export(self):
+        dummy_input = torch.randn(1, 1, 224, 224)
+        onnx_model = torch.onnx.export(self.model, dummy_input, 'model.onnx')
+    
+    
+
+
+if __name__ == '__main__':
+    
+    model = YOLO_Model(S=7,B=2,C=20)
+    model.load_state_dict(torch.load('model.pt', map_location='cpu'))
+    
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Resize([224, 224]),
+    ])
+    
+    val_ds = VOC_Dataset(image_root="/home/taozhi/datasets/VOC0712/JPEGImages/",
+                        label_root="/home/taozhi/datasets/VOC0712/Annotations/",
+                        txt_file="/home/taozhi/datasets/VOC0712/07test.txt",
+                        transform=transform)
+    
+    # val_ds = MyDataset(image_root='images',
+    #                    label_root='labels',
+    #                    txt_file='val.txt',
+    #                    transform=transform)
+    
+    
+    predictor = YOLO_Predictor(model, val_ds, score_threshold=0.2, iou_threshold=0.5, device='cpu')
+    # predictor.predict(0, path='/home/taozhi/projects/yolo/images/img (896).jpeg')
+    # predictor.save_samples(50)
+    predictor.onnx_export()
